959b0d5c8fcfc2a6537c663dad515a6778836a8a
Surinder Singh <surindersingh@ucreate.co.in>
2019-05-27 18:39:10 +0530

    add test - create user with factory

--- tests/ParentTestClass.php
+++ tests/ParentTestClass.php
@@ -7,7 +7,8 @@ class ParentTestClass extends TestCase
 {
     public static function setUpBeforeClass():void
     {
-
+        exec('php artisan migrate:refresh');
+        exec('php artisan db:seed');
     }
 
     public function tearDown():void
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class UserTest extends TestCase
 {
@@ -13,8 +14,9 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testCreateUserWithFactory()
     {
-        $this->assertTrue(true);
+        $user = factory(User::class)->create();
+        $this->assertInstanceOf(User::class, $user);
     }
 }
