f46fb00dab50ba31ba4b0e481cebafb0f592a783
Surinder Singh <surindersingh@ucreate.co.in>
2019-05-28 15:02:55 +0530

    add test to test a test havingdependency on 2 tests

--- tests/Unit/SampleTest.php
+++ tests/Unit/SampleTest.php
@@ -17,4 +17,26 @@ class SampleTest extends ParentTestClass
         $this->assertFalse(false);
         $this->assertEquals(10, 10);
     }
+
+    public function testa()
+    {
+        $this->assertTrue(true);
+        return 'a';
+    }
+
+    public function testb()
+    {
+        $this->assertTrue(true);
+        return 'b';
+    }
+
+    /**
+     * @depends testa
+     * @depends testb
+     */
+    public function testc($a, $b)
+    {
+        $this->assertEquals('a', $a);
+        $this->assertEquals('b', $b);
+    }
 }
