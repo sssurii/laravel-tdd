450a3f4595ec35d9f7b712d3f8b78f77d530c839
Surinder Singh <sssurii.dev@gmail.com>
2019-08-24 19:30:15 +0530

    update sample email command to include ics file

--- app/Console/Commands/SendEmail.php
+++ app/Console/Commands/SendEmail.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Mail;
+use INSAN\ICS\ICS;
 
 class SendEmail extends Command
 {
@@ -45,7 +46,29 @@ class SendEmail extends Command
                 $message->from(env('SENDER_EMAIL'), env('SENDER_NAME'));
                 $message->to('surindersingh@ucreate.co.in');
                 $message->subject('Thanks for joining - Email via Postmark');
+                $message->addPart($this->getICSFile([], false), 'text/calendar; charset="utf-8"; method=REQUEST');
             }
         );
+    }
+
+    private function getICSFile($mail_data, $cancel_invitation)
+    {
+        $ics_file = new ICS([
+            'uid' => uniqid(),
+            'sequence' => 0,
+            'description' => 'Event Invitation via email.',
+            'dtstart' => date('Y-m-d 09:00'),
+            'dtend' => date('Y-m-d 10:00'),
+            'summary' => 'This is an event invitation sent through email.',
+            'location' => 'VR Punjab, S.A.S Nagar, Chandigarh',
+            'url' => 'www.example.com',
+        ]);
+        //Optional
+        $ics_file->setOrganizer('Surinder', 'sssurii.dev@gmail.com');
+        //Optional
+        if ($cancel_invitation) {
+            $ics_file->markEventCancel();
+        }
+        return $ics_file->toString();
     }
 }
